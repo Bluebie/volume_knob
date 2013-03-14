f6f4348378cd6226db37d83758a6ae1b2d755176
Bluebie <a@creativepony.com>
2013-03-14 16:28:56 +1100

    Better debounce code for mute key

--- volume_knob.ino
+++ volume_knob.ino
@@ -1,5 +1,5 @@
 // a little sketch for adjusting a computer's volume level using a clicky knob
-// by Jenna Fox - MIT License
+// by @Bluebie on GitHub - MIT License
 #include "DigiKeyboard.h"
 #define KEY_MUTE 127
 #define KEY_VOLUME_UP 128
@@ -20,17 +20,34 @@ void setup() {
   digitalWrite(KNOB_QUAD_LEFT, HIGH);
   digitalWrite(KNOB_QUAD_RIGHT, HIGH);
   
-  delay(1);
+  DigiKeyboard.delay(100);
+}
+
+// debounce a pin, used for mute button
+// increase DEBOUNCE_DURATION_US if sometimes pressing mute button
+// does the action too many times.
+#define DEBOUNCE_DURATION_US 1500 /* 0.5 milliseconds */
+void debounce(byte pin, boolean state) {
+  unsigned long time_now = micros();
+  unsigned long time_since_wrong = time_now;
+  
+  while (time_since_wrong + DEBOUNCE_DURATION_US > time_now) {
+    DigiKeyboard.update(); // keep updating the keyboard so we don't crash
+    time_now = micros();
+    
+    if (digitalRead(pin) != state) {
+      time_since_wrong = time_now;
+    }
+  }
 }
 
 void loop() {
   DigiKeyboard.update();
   // check if button is pressed
   if (digitalRead(BUTTON_PIN) == LOW) {
-    delay(1); // for debouncing
     button_pressed();
-    while (digitalRead(BUTTON_PIN) == LOW) DigiKeyboard.update();
-    delay(1);
+    debounce(BUTTON_PIN, LOW); // wait till button has settled as being low
+    debounce(BUTTON_PIN, HIGH); // wait till button has settled on being high
   }
   
   alter_volume(read_knob());
